fa10ba9df316c93d6fa33e8da7261f4d1701f62d
cubeliu <cuberjhcubing@gmail.com>
2024-06-15 23:37:38 +0800

    correct spelling

--- app/src/main/java/com/chess/main/Login.java
+++ app/src/main/java/com/chess/main/Login.java
@@ -14,8 +14,8 @@ public class Login extends JPanel {
 
     final int FPS = 60;
     Mouse mouse = new Mouse();
-    JButton loginButton = new JButton("Strat Game");
-    JButton exitButton = new JButton("Quite Game");
+    JButton loginButton = new JButton("Start Game");
+    JButton exitButton = new JButton("Quit Game");
     JPanel titlePanel = new JPanel();
     static JLabel title = new JLabel("Kirby Chess Game");
 
